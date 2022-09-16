a63597a1d515787abb9d8a0457b1fe1ca08720e5
Lauryn  Kung <lkung@se14l0120-01.ad.bcit.ca>
2022-09-16 14:45:33 -0700

    new code

---
+++ week-2/test.js
@@ -0,0 +1,11 @@
+// Declaraton exercise
+// declare a variable called electronic devices
+
+//camel case
+var electronicDevices;
+
+// pascal case
+var ElectronicDevices;
+
+//Snake case
+var Electronic_Devices;
